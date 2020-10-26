b02e3e45fb5af5520b0b4903d3b8925e27361a32
kalesha-shaik <69964632+kalesha-shaik@users.noreply.github.com>
2020-10-26 20:35:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ var loadFile = function(event) {
 	var image = document.getElementById('myphoto');
 	image.src = URL.createObjectURL(event.target.files[0]);
 };
-var plants;
+var food;
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
-    plants = JSON.parse(xhttp.responseText);
+    food = JSON.parse(xhttp.responseText);
   }
 };
 xhttp.open("GET", "data.json", true);
@@ -22,13 +22,11 @@ xhttp.send();
 
 
 function taketheinput(event) {
-	// It is the javascript code for input processing ,if you press the enter button
 	if(event.key === "Enter"){
 		messages.innerHTML += user.outerHTML;
 		msgno += 1;
 		messages.lastChild.id = msgno;
 		messages.lastChild.childNodes[3].textContent = input.value;
-		//finally processes the input
 		processinput(input.value.toLowerCase());
 		input.value = "";
 
@@ -36,85 +34,61 @@ function taketheinput(event) {
 }
 
 
-function processinput(inputval){
-	if(inputval!=""){
-		messages.innerHTML += bot.outerHTML
-		msgno += 1
-		messages.lastChild.id = msgno
-		messages.lastChild.childNodes[3].textContent = reply(inputval)
-	}
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       health_issues = JSON.parse(xhttp.responseText);
+    }
+};
+xhttp.open("GET", "data.json", true);
+xhttp.send();
+
+function taketheinput(event){
+    if(event.key === "Enter"){
+        message.innerHTML += user.outerHTML;
+        msgno += 1;
+        message.lastChild.id = msg_no;
+        message.lastChild.childNodes[3].textContent = input.value;
+        processinput(input.value.toLowerCase());
+        input.value = "";
+    }
 }
-function reply(inputval) {
-	result = inputval.match(/(how)|(\w+)/g)
-	var poss = "";
-	if(poss.includes(inputval)==false && flag==false){
-		flag=true;
-		return "Hi, "+inputval.toLowerCase()+  " please enter the item you want to eat. biryani, icecream, roti, vegrice, these are available now."
-   }
-	if(result.includes("kalesha")){
-		return "please enter the item you want to eat. biryani, icecream, roti, vegrice, these are available now."
+
+function processinput(inputvalue){
+    if(inputvalue!=""){
+        message.innerHTML += bot_block.outerHTML;
+        msgno += 1;
+        message.lastChild.id = msg_no ;
+        message.lastChild.childNodes[3].textContent = botreply(inputvalue)
+    }
+}
+function botreply(inputvalue){
+    res = inputvalue.match(/(food_items)|(\w+)/g);
+    var result = "";
+    if(result.includes(inputvalue)==false && flag==false){
+        flag = true;
+        return "Hello! "+inputvalue.toUpperCase()+" Enter Your name";
+    }
+    if(res.includes("food_items yesno tables booking")){
+        return restfood.food_items;
 	}
-	if(result.includes("biryani")){
-		return "if you want to add another item type 'yes' else 'no'"
-	}
-	if(result.includes("icecream")){
-		return "if you want to add another item type 'yes' else 'no'"
-	}
-	if(result.includes("roti")){
-		return "if you want to add another item type 'yes' else 'no'"
-	}
-	if(result.includes("veg-rice")){
-		return "if you want to add another item type 'yes' else 'no'"
-	}
-	if(result.includes("yes")){
-		return "please enter the item you want to eat biryani, icecream, roti, vegrice, these are available now."
-	}
-	if(result.includes("no")){
-		return "please tell us how many are you..?  maximum limit per table is 6."
-	}
-	if(result.includes("1")){
-		return "please select a table. 10,14,20,19,26 these are availble as per your requirements"
-	}
-	if(result.includes("2")){
-		return "please select a table. 10,14,20,19,26 these are availble as per your requirements"
-	}
-	if(result.includes("3")){
-		return "please select a table. 10,14,20,19,26 these are availble as per your requirements"
-	}
-	if(result.includes("4")){
-		return "please select a table. 10,14,20,19,26 these are availble as per your requirements"
-	}
-	if(result.includes("5")){
-		return "please select a table. 10,14,20,19,26 these are availble as per your requirements"
-	}
-	if(result.includes("6")){
-		return "please select a table.10,14,20,19,26 these are availble as per your requirements"
-	}
-	if(result.includes("10")){
-		return "Thank you we booked 10 table for you. food will be prepared and bring to your table soon.";
-	}
-	if(result.includes("14")){
-		return "Thank you we booked 14 table for you. food will be prepared and bring to your table soon.";
-	}
-	if(result.includes("20")){
-		return "Thank you we booked 20 table for you. food will be prepared and bring to your table soon.";
-	}
-	if(result.includes("19")){
-		return "Thank you we booked 19 table for you. food will be prepared and bring to your table soon.";
-	}
-	if(result.includes("26")){
-		return "Thank you we booked 26 table for you. food will be prepared and bring to your table soon.";
-	}
-	if(result.includes("thankyou")){
-		return "have a great day, happy eating"
-	}
-	inp = "";
+	if(res.includes("tables")){
+        return restfood.tables;
+    }
+    
+    if(res.includes("thankyou")){
+        return "Hope I am helpful.."
+    }
+    inp = "";
     res.forEach(function(product){
-       inp= "Enter the details you want to know like foods_to_eat, foods_to_avoid ";
-       
+       if(healthIssues.hasOwnProperty(product)){
+       inp= "Enter here";
+       restfood = rest[product];
+       }
     })
-	if(inp){
-		return inp;
-	 }
+    if(inp){
+       return inp;
+    }
 	return "Hey..! i am a bot. i can not correct my self. make sure you entered correct input  :-)" ;
 }
